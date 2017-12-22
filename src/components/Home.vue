c905665f306fcd61eb2381e2ad67a18b30f95d02
JackEdwardLyons <jack.edward.lyons@gmail.com>
2017-12-22 17:46:20 +1000

    Add basic template for each page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <v-layout row>
-    <v-flex>
+  <v-layout column>
+    <v-flex xs12 class="text-xs-center" mt-5>
       <h3>Home page</h3>
+    </v-flex>
+    <v-flex class="text-xs-center" mt-5>
+      <p>This is a user's home page</p>
     </v-flex>
   </v-layout>
 </template>
+
 <script>
-export default { }
+export default {}
 </script>
